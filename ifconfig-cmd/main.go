6a35199b93060e84d0fec9d5e9ab35991304358c
Saad Guessous <guessous_saad@hotmail.com>
2022-05-12 22:25:49 +0300

    Provide args via command line

--- ifconfig-cmd/main.go
+++ ifconfig-cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"os"
 	"os/exec"
 )
@@ -21,10 +22,14 @@ func executeCommand(command string, argsArr []string) error {
 }
 
 func main() {
+	iface := flag.String("iface", "", "Interface for which you want to change the MAC")
+	newMac := flag.String("newMac", "", "Provide the new MAC address")
+	flag.Parse()
+
 	// ifconfig wlp4s0 down
-	executeCommand("sudo", []string{"ifconfig", "wlp4s0", "down"})
+	executeCommand("sudo", []string{"ifconfig", *iface, "down"})
 	// ifconfig wlp4s0 hw ether 00:11:22:33:44:55
-	executeCommand("sudo", []string{"ifconfig", "wlp4s0", "hw", "ether", "00:11:22:33:44:55"})
+	executeCommand("sudo", []string{"ifconfig", *iface, "hw", "ether", *newMac})
 	// ifconfig wlp4s0 up
-	executeCommand("sudo", []string{"ifconfig", "wlp4s0", "up"})
+	executeCommand("sudo", []string{"ifconfig", *iface, "up"})
 }
